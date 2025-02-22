3a9c0cadb3d61b4852d8328f42d5e21e33a99f8b
Rod Falanga <rfalanga@users.noreply.github.com>
2025-02-22 06:23:43 -0700

    try to commit changes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804263bb1de156291f6f40d1757b958590209d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14de41e89afaecc0b903f920194039e1b1c3792d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
