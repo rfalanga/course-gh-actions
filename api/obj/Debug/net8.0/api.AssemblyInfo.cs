a55285a27b3e1a7684226c9dbd67f20274c905f7
Rod Falanga <rfalanga@users.noreply.github.com>
2025-03-02 15:49:33 -0700

    Added .gitattribute
    and a bunch of other files, that
    I wish I had created a .gitignore
    file before getting to this point.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f12d1449bf6e17fd288774a2f9b4620841d8e7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e79d26a68373a239b145900eccec3b703fafa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
