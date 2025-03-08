9f9b088cc7b6a0b2b0734b6ee4dcc753a17211a9
Rod Falanga <rfalanga@users.noreply.github.com>
2025-03-08 06:37:28 -0700

    newsummaries

--- api/Program.cs
+++ api/Program.cs
@@ -15,7 +15,7 @@ if (app.Environment.IsDevelopment())
 
 var newSummaries = new[]
 {
-    "Brrrrr", "Yikes", "Frozen", "Hot", "Ouch", "StopIt"
+    "Brrrrr", "Frozen", "Hot", "Ouch", "StopIt"
 };
 
 var summaries = new[]
